ac2c81340713e5c04f44c75456e9bb6d98698cea
yemou <yemou@tuta.io>
2023-11-15 07:45:46 -0500

    Implement the end of statement token that was added to the SCLScanner
    Doing this lets us construct a list of statements using the tokens we
    have which can give us more opportunities to catch errors before
    trying to interpret scl files.

--- Parser.java
+++ Parser.java
@@ -7,7 +7,6 @@ import java.util.List;
 //       Currently, most checks being done are purely for the type of token we expect next rather
 //       than the token's type. This is fine for parsing some tokens but can lead to issues for
 //       operators. Some operators need two operands while others only need one.
-// TODO: Create a list of statements that we can pass to the interpreter to get output of each file
 // TODO: Consider having identifier, and other functions return their token value
 public class Parser {
   private class KeyValuePair {
@@ -20,11 +19,13 @@ public class Parser {
     }
   }
 
+  private List<Token> statementBuilder = new ArrayList<>();
+  private List<List<Token>> statements = new ArrayList<>();
+
   private List<Token> tokens;
   private int index = -1;
   private List<KeyValuePair> identifiers = new ArrayList<>();
 
-  private String tokenFoundFormatString = "Found a token with type %s and value %s";
   private boolean verbose = false;
 
   public Parser(File file) {
@@ -51,6 +52,10 @@ public class Parser {
   private Token peekNextToken() {
     if ((index + 1) < tokens.size()) return tokens.get(index + 1);
     else return null;
+  }
+
+  public List<List<Token>> getStatements() {
+    return statements;
   }
 
   public boolean identifierExists(String identifier) {
@@ -120,21 +125,26 @@ public class Parser {
     }
   }
 
+  private void foundToken(Token token) {
+    log(String.format("Found a token with type %s and value %s", token.TYPE, token.VALUE));
+    statementBuilder.add(token);
+  }
+
   private void imports(Token token) {
     log("Entering imports");
     expectOrError(TokenType.KEYWORD, "import", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting a literal next");
+    log("Expecting a literal");
     literal(getNextToken());
   }
 
   private void literal(Token token) {
     log("Entering literal");
     expectOrError(TokenType.LITERAL, token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting operator, special_symbol, or nothing next");
+    log("Expecting an operator, special_symbol, end of statement, or nothing");
 
     Token nextToken = peekNextToken();
     if (nextToken == null) return;
@@ -146,28 +156,42 @@ public class Parser {
       case SPECIAL_SYMBOL:
         special_symbol(getNextToken());
         break;
+      case END_OF_STATEMENT:
+        eos(getNextToken());
+        break;
       default:
         break;
     }
   }
 
+  private void eos(Token token) {
+    log("Entering end of statement");
+    expectOrError(TokenType.END_OF_STATEMENT, token);
+    foundToken(token);
+
+    statements.add(statementBuilder);
+    statementBuilder = new ArrayList<>();
+  }
+
   private void symbols(Token token) {
     log("Entering symbols");
     expectOrError(TokenType.KEYWORD, "symbol", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting an identifier next");
+    log("Expecting an identifier");
     identifier(getNextToken());
   }
 
   private void identifier(Token token) {
     log("Entering identifiers");
     expectOrError(TokenType.IDENTIFIER, token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
     // TODO: Add all identifiers and their values to some sort of datastructure so they can be
-    // recalled later.
-    log("Expecting a literal, constant, operator, special_symbol, or nothing next");
+    //       recalled later.
+    //       This may have to be in a second pass when we have the values and the identifier
+    //       together. Currently we can only check if we have already encountered an identifier.
+    log("Expecting a literal, constant, operator, special_symbol, end of statement, or nothing");
 
     Token nextToken = peekNextToken();
     if (nextToken == null) return;
@@ -185,6 +209,9 @@ public class Parser {
       case SPECIAL_SYMBOL:
         special_symbol(getNextToken());
         break;
+      case END_OF_STATEMENT:
+        eos(getNextToken());
+        break;
       default:
         break;
     }
@@ -193,9 +220,9 @@ public class Parser {
   private void constant(Token token) {
     log("Entering constants");
     expectOrError(TokenType.CONSTANT, token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting operator, special_symbol, or nothing next");
+    log("Expecting operator, special_symbol, or nothing");
 
     Token nextToken = peekNextToken();
     if (nextToken == null) return;
@@ -207,6 +234,9 @@ public class Parser {
       case SPECIAL_SYMBOL:
         special_symbol(getNextToken());
         break;
+      case END_OF_STATEMENT:
+        eos(getNextToken());
+        break;
       default:
         break;
     }
@@ -217,9 +247,9 @@ public class Parser {
   private void operator(Token token) {
     log("Entering operator");
     expectOrError(TokenType.OPERATOR, token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting a literal, constant, identifier, operator, or special_symbol next");
+    log("Expecting a literal, constant, identifier, operator, or special_symbol");
 
     Token nextToken = getNextToken();
     switch (nextToken.TYPE) {
@@ -248,9 +278,9 @@ public class Parser {
   private void special_symbol(Token token) {
     log("Entering special_symbol");
     expectOrError(TokenType.SPECIAL_SYMBOL, token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting literal, constant, identifier, or nothing next");
+    log("Expecting literal, constant, identifier, or nothing");
 
     Token nextToken = peekNextToken();
     if (nextToken == null) return;
@@ -265,6 +295,9 @@ public class Parser {
       case IDENTIFIER:
         identifier(getNextToken());
         break;
+      case END_OF_STATEMENT:
+        eos(getNextToken());
+        break;
       default:
         break;
     }
@@ -273,28 +306,35 @@ public class Parser {
   private void globals(Token token) {
     log("Entering globals");
     expectOrError(TokenType.KEYWORD, "global", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting declarations next");
+    log("Expecting declarations");
     declarations(getNextToken());
   }
 
   private void declarations(Token token) {
     log("Entering declarations");
     expectOrError(TokenType.KEYWORD, "declarations", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting variables next");
+    log("Expecting end of statement");
+    eos(getNextToken());
+
+    log("Back in declarations");
+    log("Expecting variables");
     variables(getNextToken());
   }
 
   private void variables(Token token) {
     log("Entering variables");
     expectOrError(TokenType.KEYWORD, "variables", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting define next");
+    log("Expecting end of statement");
+    eos(getNextToken());
 
+    log("Back in variables");
+    log("Expecting defines");
     Token nextToken = peekNextToken();
     while (nextToken != null
         && !expect(TokenType.KEYWORD, "implementations", nextToken)
@@ -307,31 +347,31 @@ public class Parser {
   private void define(Token token) {
     log("Entering define");
     expectOrError(TokenType.KEYWORD, "define", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting an identifier next");
+    log("Expecting an identifier");
     identifier(getNextToken());
-    log("Back in define");
 
-    log("Expecting of next");
+    log("Back in define");
+    log("Expecting of");
     of(getNextToken());
   }
 
   private void of(Token token) {
     log("Entering of");
     expectOrError(TokenType.KEYWORD, "of", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting type next");
+    log("Expecting type");
     type(getNextToken());
   }
 
   private void type(Token token) {
     log("Entering type");
     expectOrError(TokenType.KEYWORD, "type", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting either unsigned, integer, short, long, or byte next");
+    log("Expecting either unsigned, integer, short, long, or byte");
 
     Token nextToken = getNextToken();
     switch (nextToken.VALUE) {
@@ -360,9 +400,9 @@ public class Parser {
   private void unsigned(Token token) {
     log("Entering unsigned");
     expectOrError(TokenType.KEYWORD, "unsigned", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting either integer, short, or long next");
+    log("Expecting either integer, short, or long");
 
     Token nextToken = getNextToken();
     switch (nextToken.VALUE) {
@@ -385,77 +425,95 @@ public class Parser {
   private void integer(Token token) {
     log("Entering integer");
     expectOrError(TokenType.KEYWORD, "integer", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
+    log("Expecting end of statement");
+    eos(getNextToken());
   }
 
   private void _short(Token token) {
     log("Entering short");
     expectOrError(TokenType.KEYWORD, "short", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
+    log("Expecting end of statement");
+    eos(getNextToken());
   }
 
   private void _long(Token token) {
     log("Entering long");
     expectOrError(TokenType.KEYWORD, "long", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
+    log("Expecting end of statement");
+    eos(getNextToken());
   }
 
   private void _byte(Token token) {
     log("Entering byte");
     expectOrError(TokenType.KEYWORD, "byte", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
+    log("Expecting end of statement");
+    eos(getNextToken());
   }
 
   private void implementation(Token token) {
     log("Entering implementations");
     expectOrError(TokenType.KEYWORD, "implementations", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting function next");
+    log("Expecting end of statement");
+    eos(getNextToken());
+
+    log("Back in implementations");
+    log("Expecting function");
     function(getNextToken());
   }
 
   private void function(Token token) {
     log("Entering function");
     expectOrError(TokenType.KEYWORD, "function", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting identifer next");
+    log("Expecting identifer");
     identifier(getNextToken());
 
     log("Back in function");
-    log("Expecting is next");
+    log("Expecting is");
     is(getNextToken());
 
     log("Back in function");
-    log("Expecting endfun next");
+    log("Expecting endfun");
     endfun(getNextToken());
   }
 
   private void is(Token token) {
     log("Entering is");
     expectOrError(TokenType.KEYWORD, "is", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting variables next");
+    log("Expecting end of statement");
+    eos(getNextToken());
+
+    log("Expecting variables");
     variables(getNextToken());
 
     log("Back in is");
-    log("Expecting begin next");
+    log("Expecting begin");
     _begin(getNextToken());
   }
 
   private void _begin(Token token) {
     log("Entering begin");
     expectOrError(TokenType.KEYWORD, "begin", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting either set, display, or exit next");
+    log("Expecting end of statement");
+    eos(getNextToken());
 
+    log("Back in begin");
+    log("Expecting either set, display, or exit");
     Token nextToken = peekNextToken();
     while (nextToken != null && !expect(TokenType.KEYWORD, "endfun", nextToken)) {
       switch (nextToken.VALUE) {
@@ -481,19 +539,18 @@ public class Parser {
   private void set(Token token) {
     log("Entering set");
     expectOrError(TokenType.KEYWORD, "set", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting an identifier next");
+    log("Expecting an identifier");
     identifier(getNextToken());
   }
 
   private void display(Token token) {
     log("Entering display");
     expectOrError(TokenType.KEYWORD, "display", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
-    log("Expecting an identifier or literal next");
-
+    log("Expecting an identifier or literal");
     Token nextToken = getNextToken();
 
     switch (nextToken.TYPE) {
@@ -513,15 +570,17 @@ public class Parser {
   private void exit(Token token) {
     log("Entering exit");
     expectOrError(TokenType.KEYWORD, "exit", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
+    log("Expecting end of statement");
+    eos(getNextToken());
   }
 
   private void endfun(Token token) {
     log("Entering endfun");
     expectOrError(TokenType.KEYWORD, "endfun", token);
+    foundToken(token);
 
-    log(String.format(tokenFoundFormatString, token.TYPE, token.VALUE));
     log("Expecting identifier next");
     identifier(getNextToken());
   }
@@ -535,7 +594,13 @@ public class Parser {
     String filename = args[0];
     File file = new File(filename);
 
-    Parser parser = new Parser(file, true);
+    Parser parser = new Parser(file);
     parser.begin();
+
+    List<List<Token>> statements = parser.getStatements();
+    for (List<Token> statement : statements) {
+      for (Token token : statement) System.out.print(token.TYPE + ":" + token.VALUE + " ");
+      System.out.println();
+    }
   }
 }
