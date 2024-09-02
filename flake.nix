3b1de1db6658826ee91824f2715e409caa367252
yemou <dev@lilac.pink>
2024-09-01 21:15:47 -0400

    Update flake.nix
    You can now build and run the project using the usual nix commands

--- flake.nix
+++ flake.nix
@@ -14,7 +14,25 @@
         maven
       ];
     };
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-    # overlays.default = final: prev: { };
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.maven.buildMavenPackage rec {
+      pname = "SCLInterpreter";
+      version = "1.0.0";
+      src = ./.;
+
+      mvnHash = "sha256-8WgNz6qOeBqmT2tfM4puTZ1mUgE0TsqsSTMOtYwHBrc=";
+      nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [ makeWrapper ];
+
+      installPhase = ''
+        mkdir -p $out/bin $out/share/${pname}
+        install -Dm644 target/scli-${version}.jar $out/share/${pname}
+
+        makeWrapper ${nixpkgs.legacyPackages.x86_64-linux.jre17_minimal}/bin/java $out/bin/scli \
+          --add-flags "-jar $out/share/${pname}/scli-${version}.jar"
+      '';
+    };
+    apps.x86_64-linux.default = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.default}/bin/scli";
+    };
   };
 }
